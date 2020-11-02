87c7a046417db41ca51e9d1b0350205ac6a74874
SparshJain2000 <jainsparsh0801@gmail.com>
2020-11-03 01:11:20 +0530

    rename 1230A : david and bag of candies

---
+++ A/1230A.cpp
@@ -0,0 +1,27 @@
+/*David and bag of candies*/
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+int main()
+{
+
+    std::vector<long> a(4);
+    for (long p = 0; p < 4; p++)
+    {
+        scanf("%ld", &a[p]);
+    }
+    sort(a.begin(), a.end());
+    bool possible(false);
+    if (a[3] == a[0] + a[1] + a[2])
+    {
+        possible = true;
+    }
+    else if (a[0] + a[3] == a[1] + a[2])
+    {
+        possible = true;
+    }
+    puts(possible ? "YES" : "NO");
+
+    return 0;
+}
